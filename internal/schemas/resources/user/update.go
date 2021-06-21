d9263d659a70813dccfef2406bfba6ab6d57d652
Joaquim Magalhães <10578843+joaquimmmagalhaes@users.noreply.github.com>
2021-06-21 23:46:37 +0100

    fix update fields

--- internal/schemas/resources/user/update.go
+++ internal/schemas/resources/user/update.go
@@ -13,11 +13,23 @@ func update(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Dia
 	adminID := d.Id()
 
 	if d.HasChanges("status") ||
+		d.HasChanges("username") ||
 		d.HasChanges("description") ||
+		d.HasChanges("expiration_date") ||
 		d.HasChanges("password") ||
-		d.HasChanges("email") ||
+		d.HasChanges("public_keys") ||
+		d.HasChanges("home_dir") ||
 		d.HasChanges("permissions") ||
+		d.HasChanges("uid") ||
+		d.HasChanges("gid") ||
+		d.HasChanges("max_sessions") ||
+		d.HasChanges("quota_size") ||
+		d.HasChanges("quota_files") ||
+		d.HasChanges("virtual_folders") ||
+		d.HasChanges("upload_bandwidth") ||
+		d.HasChanges("download_bandwidth") ||
 		d.HasChanges("filters") ||
+		d.HasChanges("filesystem") ||
 		d.HasChanges("additional_info") {
 		err := c.UpdateUser(ctx, adminID, convertToStruct(d))
 		if err != nil {
